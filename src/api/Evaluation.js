85549251dd4b13ebcc383b793ffec95981c51ebd
jiushi <2378166881@qq.com>
2024-11-15 14:42:11 +0800

    互评

---
+++ src/api/Evaluation.js
@@ -0,0 +1,30 @@
+import request from '../utils/request'
+
+//展示课程作业信息
+export const getCourseHomework = ( reviewerSno, cid, workid ) =>
+    request.post('/api/student/peerDisplay', { reviewerSno, cid, workid} )
+
+
+
+
+// 课程作业下载
+export const peerDownload = (reviewersno) => {
+    return request.get('/api/student/peerDownload',
+
+        {reviewersno})
+}
+
+// 提交作业评分
+export const submitReview = (reviewData) => {
+    return request.post('/api/student/submitReview', reviewData)
+}
+
+// 修改作业评分
+export const modifyReview = ( reviewData) => {
+    return request.post('/api/student/modifyReview', reviewData)
+}
+
+// 展示作业评分
+export const viewPeerReviews = (reviewData) => {
+    return request.post('/api/student/viewPeerReviews', reviewData)
+}
